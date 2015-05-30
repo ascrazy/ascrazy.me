8f6db8178c50f6e4fdbd4f864f87b7cbbcceea54
Alexandr Shahorsky <alexandr@ascrazy.me>
2015-05-30 08:26:06 +0000

    Improve release workflow with assets minification and revisions tracking. Use Ubuntu font family on the site

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,23 @@
 var del          = require('del');
 var gulp         = require('gulp');
 var gutil        = require('gulp-util');
+var gulpif       = require('gulp-if');
 var runSequence  = require('run-sequence');
 var rename       = require('gulp-rename');
+var sourcemaps   = require('gulp-sourcemaps');
 var livereload   = require('gulp-livereload');
 var plumber      = require('gulp-plumber');
 var sftp         = require('gulp-sftp');
+var rev          = require('gulp-rev');
+var revReplace   = require('gulp-rev-replace');
 var sass         = require('gulp-sass');
+var postcss      = require('gulp-postcss')
+var autoprefixer = require('autoprefixer-core')
+var minifyCss    = require('gulp-minify-css');
 var source       = require('vinyl-source-stream');
+var buffer       = require('vinyl-buffer');
 var browserify   = require('browserify');
+var uglify       = require('gulp-uglify');
 
 function swallowError (error) {
   gutil.log(gutil.colors.yellow(error.toString()));
@@ -22,7 +31,12 @@ gulp.task('scripts', function(done) {
   }).bundle().on('error', swallowError);
 
   bundler.pipe(source('bundle.js'))
+    .pipe(plumber(swallowError))
     .pipe(rename('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(gulpif(process.env.NODE_ENV === "production", uglify()))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build/scripts/'))
     .on('finish', function(){
       livereload.changed('scripts/main.js');
@@ -35,6 +49,8 @@ gulp.task('styles', function(done) {
     .pipe(plumber(swallowError))
     .pipe(sass())
     .pipe(rename('main.css'))
+    .pipe(postcss([ autoprefixer({ "browsers": [ 'last 2 version' ] })]))
+    .pipe(gulpif(process.env.NODE_ENV === "production", minifyCss()))
     .pipe(gulp.dest('build/styles'))
     .on("finish", function(){
       livereload.changed('styles/main.css');
@@ -60,15 +76,11 @@ gulp.task('copy', function(done) {
     });
 });
 
-gulp.task('clean', function(done){
-  del('build/*', done);
-});
-
-gulp.task('build', ['clean'], function(done){
+gulp.task('build-all', [ 'clean' ], function(done){
   runSequence(['scripts', 'styles', 'images', 'copy'], done);
 });
 
-gulp.task('serve', [ 'build' ], function(done){
+gulp.task('serve', [ 'build-all' ], function(done){
   gulp.watch('src/scripts/**/*.{js,jsx}',  [ 'scripts' ]);
   gulp.watch('src/styles/**/*.{css,scss}', [ 'styles' ]);
   gulp.watch('src/*.{html,txt,xml}',       [ 'copy' ]);
@@ -83,13 +95,31 @@ gulp.task('serve', [ 'build' ], function(done){
   });
 });
 
-gulp.task('deploy', [ 'build' ], function(done){
-  gulp.src('build/**/*')
+gulp.task('release-manifest', [ 'build-all' ], function(){
+  return gulp.src('build/**/*.{js,css}')
+    .pipe(rev())
+    .pipe(gulp.dest('build/'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('release', [ 'release-manifest' ], function(){
+  var manifest = gulp.src('build/rev-manifest.json');
+  return gulp.src('build/**/*.html')
+    .pipe(revReplace({manifest: manifest}))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('deploy', [ 'release' ], function(){
+  return gulp.src('build/**/*')
     .pipe(sftp({
       host:       'death-star.ascrazy.me',
       user:       'root',
       remotePath: '/var/www/ascrazy.me',
       agent:      process.env.SSH_AUTH_SOCK
-    }))
-    .on("finish", done);
+    }));
+});
+
+gulp.task('clean', function(done){
+  del('build/*', done);
 });
