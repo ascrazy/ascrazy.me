7816a6943d3f4f08cf9bc8f84597237fdf15bca2
Alexander Shahorsky <i.alexandr.sh@gmail.com>
2024-01-15 16:17:49 +0000

    Add decoupled build and serve commands

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,12 +1,17 @@
+import { promises as fs } from 'fs'
 import { renderToString } from "react-dom/server";
 import type { ResumeType } from "./ResumeType";
 import { Resume } from "./Resume";
 
 export async function render(resume: ResumeType) {
+    const css = await fs.readFile(import.meta.dir + '/style.css', 'utf-8')
     return await renderToString(
         <html>
             <head>
                 <title>{resume.basics.name}</title>
+                <style>
+                    {css}                    
+                </style>
             </head>
             <body>
                 <Resume resume={resume} />
